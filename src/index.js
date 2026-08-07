afcbd4f65c05fe88149b10e861e967d774032e9b
Vaishnavi Parab <your-github-email@example.com>
2026-08-07 14:58:25 +0530

    Add Google Analytics

--- src/index.js
+++ src/index.js
@@ -3,16 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga4';
+
+// Initialize Google Analytics
+ReactGA.initialize('G-6VBRLSQTZ5');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Send page view
+ReactGA.send({
+  hitType: 'pageview',
+  page: window.location.pathname,
+});
 
+reportWebVitals();
